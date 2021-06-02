c25f470be193d1d826ef05500ff25fcd1703586c
Rafa Lynch <rafa_lynch@hotmail.com>
2021-06-02 01:24:37 -0300

    mobile fixes and nav-bar changed

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const navSlide = () => {
   const burger = document.querySelector(".burger");
   const nav = document.querySelector(".nav-links");
   const navLinks = document.querySelectorAll(".nav-links li");
+  const body = document.querySelector("body");
 
   burger.addEventListener("click", () => {
     // toggle nav-bar
@@ -11,10 +12,12 @@ const navSlide = () => {
       link.style.animation = "navLinkFade 0.5s ease forwards";
     });
     burger.classList.toggle("toggle");
+    body.classList.toggle("body-stuck");
   });
 
   navLinks.forEach((link, index) => {
     link.addEventListener("click", () => {
+      body.classList.toggle("body-stuck");
       nav.classList.toggle("nav-active");
       burger.classList.toggle("toggle");
     });
